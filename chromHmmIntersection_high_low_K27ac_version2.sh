9d730a6f362973ff703ddf0a2fd33bd6f8033b21
Sowmya Iyer <sowmyai@bu.edu>
2014-09-11 13:42:57 -0400

    changes to add chromHmm annotations to ChIP-seq intersections

--- chromHmmIntersection_high_low_K27ac_version2.sh
+++ chromHmmIntersection_high_low_K27ac_version2.sh
@@ -1,33 +1,53 @@
 for cell in {"GM12878","HepG2","K562","endothelial.cell.of.umbilical.vein","H1-hESC","HeLa-S3"}
 do
 	echo $cell
-	Rscript getBackgroundStats.R ${cell}
+	Rscript separateHighLowH3K27ac.R ${cell}
 	
 	bedtools intersect -a dnase_${cell}.combined.merged.distal.high_H3K27ac.narrowPeak -b wgEncodeAwgSegmentationChromhmm.${cell}.bed -wao | sort -k1,1V -k2,2n | bedtools groupby -c 9,15 -o collapse,collapse > ${cell}_dnase_high_H3K27ac_intersect_chromHmm.25.states.distal_v2.txt
 	awk -f pickState.awk ${cell}_dnase_high_H3K27ac_intersect_chromHmm.25.states.distal_v2.txt | sort |  uniq -c | awk '{ print $2"\t"$1}' > ${cell}_dnase_high_H3K27ac_intersect_chromHmm.25.states.distal.counts.txt
+	
+	bedtools intersect -a dnase_${cell}.combined.merged.distal.high_H3K27ac.narrowPeak -b allchipseq/all_chipseq_peaks.${cell}.bed.sorted.merged -wa | bedtools intersect -a stdin -b  wgEncodeAwgSegmentationChromhmm.${cell}.bed -wao | sort -k1,1V -k2,2n | bedtools groupby -c 9,15 -o collapse,collapse > ${cell}_dnase_high_H3K27ac_intersect_chipSeq_intersect_chromHmm.25.states.distal_v2.txt
+        awk -f pickState.awk ${cell}_dnase_high_H3K27ac_intersect_chipSeq_intersect_chromHmm.25.states.distal_v2.txt | sort |  uniq -c | awk '{ print $2"\t"$1}' > ${cell}_dnase_high_H3K27ac_intersect_chipSeq_intersect_chromHmm.25.states.distal.counts.txt
 
 	bedtools intersect -a dnase_${cell}.combined.merged.distal.low_H3K27ac.narrowPeak -b wgEncodeAwgSegmentationChromhmm.${cell}.bed -wao | sort -k1,1V -k2,2n | bedtools groupby -c 9,15 -o collapse,collapse > ${cell}_dnase_low_H3K27ac_intersect_chromHmm.25.states.distal_v2.txt
 	awk -f pickState.awk ${cell}_dnase_low_H3K27ac_intersect_chromHmm.25.states.distal_v2.txt | sort |  uniq -c | awk '{ print $2"\t"$1}' > ${cell}_dnase_low_H3K27ac_intersect_chromHmm.25.states.distal.counts.txt
+	bedtools intersect -a dnase_${cell}.combined.merged.distal.low_H3K27ac.narrowPeak -b allchipseq/all_chipseq_peaks.${cell}.bed.sorted.merged -wa | bedtools intersect -a stdin -b  wgEncodeAwgSegmentationChromhmm.${cell}.bed -wao | sort -k1,1V -k2,2n | bedtools groupby -c 9,15 -o collapse,collapse > ${cell}_dnase_low_H3K27ac_intersect_chipSeq_intersect_chromHmm.25.states.distal_v2.txt
+        awk -f pickState.awk ${cell}_dnase_low_H3K27ac_intersect_chipSeq_intersect_chromHmm.25.states.distal_v2.txt | sort |  uniq -c | awk '{ print $2"\t"$1}' > ${cell}_dnase_low_H3K27ac_intersect_chipSeq_intersect_chromHmm.25.states.distal.counts.txt
 
 	bedtools intersect -a random_${cell}.combined.merged.distal.high_H3K27ac.narrowPeak -b wgEncodeAwgSegmentationChromhmm.${cell}.bed -wao | sort -k1,1V -k2,2n | bedtools groupby -c 9,15 -o collapse,collapse > ${cell}_random_high_H3K27ac_intersect_chromHmm.25.states.distal_v2.txt
 	awk -f pickState.awk ${cell}_random_high_H3K27ac_intersect_chromHmm.25.states.distal_v2.txt | sort |  uniq -c | awk '{ print $2"\t"$1}' > ${cell}_random_high_H3K27ac_intersect_chromHmm.25.states.distal.counts.txt
+	bedtools intersect -a random_${cell}.combined.merged.distal.high_H3K27ac.narrowPeak -b allchipseq/all_chipseq_peaks.${cell}.bed.sorted.merged -wa | bedtools intersect -a stdin -b  wgEncodeAwgSegmentationChromhmm.${cell}.bed -wao | sort -k1,1V -k2,2n | bedtools groupby -c 9,15 -o collapse,collapse > ${cell}_random_high_H3K27ac_intersect_chipSeq_intersect_chromHmm.25.states.distal_v2.txt
+        awk -f pickState.awk ${cell}_random_high_H3K27ac_intersect_chipSeq_intersect_chromHmm.25.states.distal_v2.txt | sort |  uniq -c | awk '{ print $2"\t"$1}' > ${cell}_random_high_H3K27ac_intersect_chipSeq_intersect_chromHmm.25.states.distal.counts.txt
+
+
  	
 	bedtools intersect -a random_${cell}.combined.merged.distal.low_H3K27ac.narrowPeak -b wgEncodeAwgSegmentationChromhmm.${cell}.bed -wao | sort -k1,1V -k2,2n | bedtools groupby -c 9,15 -o collapse,collapse > ${cell}_random_low_H3K27ac_intersect_chromHmm.25.states.distal_v2.txt
 	awk -f pickState.awk ${cell}_random_low_H3K27ac_intersect_chromHmm.25.states.distal_v2.txt | sort |  uniq -c | awk '{ print $2"\t"$1}' > ${cell}_random_low_H3K27ac_intersect_chromHmm.25.states.distal.counts.txt
-
+	bedtools intersect -a random_${cell}.combined.merged.distal.low_H3K27ac.narrowPeak -b allchipseq/all_chipseq_peaks.${cell}.bed.sorted.merged -wa | bedtools intersect -a stdin -b  wgEncodeAwgSegmentationChromhmm.${cell}.bed -wao | sort -k1,1V -k2,2n | bedtools groupby -c 9,15 -o collapse,collapse > ${cell}_random_low_H3K27ac_intersect_chipSeq_intersect_chromHmm.25.states.distal_v2.txt
+        awk -f pickState.awk ${cell}_random_low_H3K27ac_intersect_chipSeq_intersect_chromHmm.25.states.distal_v2.txt | sort |  uniq -c | awk '{ print $2"\t"$1}' > ${cell}_random_low_H3K27ac_intersect_chipSeq_intersect_chromHmm.25.states.distal.counts.txt
 
 
 	bedtools intersect -a dnase_${cell}.combined.merged.proximal.high_H3K27ac.narrowPeak -b wgEncodeAwgSegmentationChromhmm.${cell}.bed -wao | sort -k1,1V -k2,2n | bedtools groupby -c 9,15 -o collapse,collapse > ${cell}_dnase_high_H3K27ac_intersect_chromHmm.25.states.proximal_v2.txt
 	awk -f pickState.awk ${cell}_dnase_high_H3K27ac_intersect_chromHmm.25.states.proximal_v2.txt | sort |  uniq -c | awk '{ print $2"\t"$1}' > ${cell}_dnase_high_H3K27ac_intersect_chromHmm.25.states.proximal.counts.txt
+	bedtools intersect -a dnase_${cell}.combined.merged.proximal.high_H3K27ac.narrowPeak -b allchipseq/all_chipseq_peaks.${cell}.bed.sorted.merged -wa | bedtools intersect -a stdin -b  wgEncodeAwgSegmentationChromhmm.${cell}.bed -wao | sort -k1,1V -k2,2n | bedtools groupby -c 9,15 -o collapse,collapse > ${cell}_dnase_high_H3K27ac_intersect_chipSeq_intersect_chromHmm.25.states.proximal_v2.txt
+        awk -f pickState.awk ${cell}_dnase_high_H3K27ac_intersect_chipSeq_intersect_chromHmm.25.states.proximal_v2.txt | sort |  uniq -c | awk '{ print $2"\t"$1}' > ${cell}_dnase_high_H3K27ac_intersect_chipSeq_intersect_chromHmm.25.states.proximal.counts.txt
+
 
 	bedtools intersect -a dnase_${cell}.combined.merged.proximal.low_H3K27ac.narrowPeak -b wgEncodeAwgSegmentationChromhmm.${cell}.bed -wao | sort -k1,1V -k2,2n | bedtools groupby -c 9,15 -o collapse,collapse > ${cell}_dnase_low_H3K27ac_intersect_chromHmm.25.states.proximal_v2.txt
 	awk -f pickState.awk ${cell}_dnase_low_H3K27ac_intersect_chromHmm.25.states.proximal_v2.txt | sort |  uniq -c | awk '{ print $2"\t"$1}' > ${cell}_dnase_low_H3K27ac_intersect_chromHmm.25.states.proximal.counts.txt
+	bedtools intersect -a dnase_${cell}.combined.merged.proximal.low_H3K27ac.narrowPeak -b allchipseq/all_chipseq_peaks.${cell}.bed.sorted.merged -wa | bedtools intersect -a stdin -b  wgEncodeAwgSegmentationChromhmm.${cell}.bed -wao | sort -k1,1V -k2,2n | bedtools groupby -c 9,15 -o collapse,collapse > ${cell}_dnase_low_H3K27ac_intersect_chipSeq_intersect_chromHmm.25.states.proximal_v2.txt
+        awk -f pickState.awk ${cell}_dnase_low_H3K27ac_intersect_chipSeq_intersect_chromHmm.25.states.proximal_v2.txt | sort |  uniq -c | awk '{ print $2"\t"$1}' > ${cell}_dnase_low_H3K27ac_intersect_chipSeq_intersect_chromHmm.25.states.proximal.counts.txt
+
 
 	bedtools intersect -a random_${cell}.combined.merged.proximal.high_H3K27ac.narrowPeak -b wgEncodeAwgSegmentationChromhmm.${cell}.bed -wao | sort -k1,1V -k2,2n | bedtools groupby -c 9,15 -o collapse,collapse > ${cell}_random_high_H3K27ac_intersect_chromHmm.25.states.proximal_v2.txt
 	awk -f pickState.awk ${cell}_random_high_H3K27ac_intersect_chromHmm.25.states.proximal_v2.txt | sort |  uniq -c | awk '{ print $2"\t"$1}' > ${cell}_random_high_H3K27ac_intersect_chromHmm.25.states.proximal.counts.txt
+	bedtools intersect -a random_${cell}.combined.merged.proximal.high_H3K27ac.narrowPeak -b allchipseq/all_chipseq_peaks.${cell}.bed.sorted.merged -wa | bedtools intersect -a stdin -b  wgEncodeAwgSegmentationChromhmm.${cell}.bed -wao | sort -k1,1V -k2,2n | bedtools groupby -c 9,15 -o collapse,collapse > ${cell}_random_high_H3K27ac_intersect_chipSeq_intersect_chromHmm.25.states.proximal_v2.txt
+        awk -f pickState.awk ${cell}_random_high_H3K27ac_intersect_chipSeq_intersect_chromHmm.25.states.proximal_v2.txt | sort |  uniq -c | awk '{ print $2"\t"$1}' > ${cell}_random_high_H3K27ac_intersect_chipSeq_intersect_chromHmm.25.states.proximal.counts.txt
  	
 	bedtools intersect -a random_${cell}.combined.merged.proximal.low_H3K27ac.narrowPeak -b wgEncodeAwgSegmentationChromhmm.${cell}.bed -wao | sort -k1,1V -k2,2n | bedtools groupby -c 9,15 -o collapse,collapse > ${cell}_random_low_H3K27ac_intersect_chromHmm.25.states.proximal_v2.txt
 	awk -f pickState.awk ${cell}_random_low_H3K27ac_intersect_chromHmm.25.states.proximal_v2.txt | sort |  uniq -c | awk '{ print $2"\t"$1}' > ${cell}_random_low_H3K27ac_intersect_chromHmm.25.states.proximal.counts.txt
+	bedtools intersect -a random_${cell}.combined.merged.proximal.low_H3K27ac.narrowPeak -b allchipseq/all_chipseq_peaks.${cell}.bed.sorted.merged -wa | bedtools intersect -a stdin -b  wgEncodeAwgSegmentationChromhmm.${cell}.bed -wao | sort -k1,1V -k2,2n | bedtools groupby -c 9,15 -o collapse,collapse > ${cell}_random_low_H3K27ac_intersect_chipSeq_intersect_chromHmm.25.states.proximal_v2.txt
+        awk -f pickState.awk ${cell}_random_low_H3K27ac_intersect_chipSeq_intersect_chromHmm.25.states.proximal_v2.txt | sort |  uniq -c | awk '{ print $2"\t"$1}' > ${cell}_random_low_H3K27ac_intersect_chipSeq_intersect_chromHmm.25.states.proximal.counts.txt
 
 	
 	for f in ./${cell}_*H3K27ac_intersect_chromHmm.25.states*counts.txt
@@ -36,5 +56,13 @@ do
 		mv $f.tmp $f
 		wc -l $f
 	done
+        for f in ./${cell}_*H3K27ac_intersect_chipSeq_intersect_chromHmm.25.states*counts.txt
+        do
+		
+                sort -k1,1 $f | join states.26.txt - -a 1 | awk -F" " '{ if (NF == 1) {print $1"\t"0} else {print $1"\t"$2}}' > $f.tmp
+                mv $f.tmp $f
+                wc -l $f
+        done
 	Rscript chromHmmIntersection_high_low_K27ac_version2.R ${cell}
 done
+
